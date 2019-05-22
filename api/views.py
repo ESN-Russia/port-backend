076e367466ea8d7839f4629f63d303049ac71199
Andrey Sokolov <andresokol@yandex-team.ru>
2019-05-23 01:12:40 +0300

    fix

--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response as RestResponse
 
 import requests
 import json
+import xmltodict
 
 from .serializers import *
 
@@ -46,6 +47,8 @@ def validate_ticket(request: HttpRequest, token_id):
         'service': f'{CAS_CALLBACK}/{token_id}/',
         'ticket': ticket
     })
+    data = xmltodict.parse(d.content)
+    print(data)
     username = d.text.split()[0]
     token = EsnAuthToken.objects.get(nanoid=token_id)
     token.username = username
